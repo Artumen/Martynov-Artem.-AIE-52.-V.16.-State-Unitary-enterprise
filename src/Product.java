8788da5456c3ebfb03617b3093df1eb6fbfcf1ec
Artumen <969artumen123@mail.ru>
2026-05-31 01:36:45 +0700

    Product.java

--- src/Product.java
+++ src/Product.java
@@ -1,16 +1,21 @@
 public abstract class Product {
+//Название 
     private String name;
+//количество/вес продукта
     private double amount;
 
+//конструктор 
     public Product(String name, double amount) {
         this.name = name;
         this.amount = amount;
     }
 
+//Получить название
     public String getName() {
         return name;
     }
 
+//Получить количество,вес
     public double getAmount() {
         return amount;
     }
