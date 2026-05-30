d894e48dda19c23e51c417da432465ba17bed26e
Artumen <969artumen123@mail.ru>
2026-05-31 01:38:44 +0700

    Wheat.java

--- src/Wheat.java
+++ src/Wheat.java
@@ -1,17 +1,22 @@
 public class Wheat extends Product {
+//Твёрдость пшеницы
     private String hardness;
+//Класс качества
     private String classGrade;
 
+//Конструктор 
     public Wheat(double amount, String hardness, String classGrade) {
-        super("Пшеница", amount);
+        super("Пшеница", amount); 
         this.hardness = hardness;
         this.classGrade = classGrade;
     }
 
+//Получить твердость
     public String getHardness() {
         return hardness;
     }
 
+//получить класс качества
     public String getClassGrade() {
         return classGrade;
     }
