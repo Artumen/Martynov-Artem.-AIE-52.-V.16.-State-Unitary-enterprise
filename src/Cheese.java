c248a4ef8d0a4eeb6056645d1e27cb52997cb414
Artumen <969artumen123@mail.ru>
2026-05-31 01:29:48 +0700

    Update Cheese.java
    Сыр

--- src/Cheese.java
+++ src/Cheese.java
@@ -1,24 +1,30 @@
-
 public class Cheese extends Product {
+// Сорт
     private String sort;
+//Процент жирности
     private int fatPercent;
+//Тип упаковки
     private String packaging;
 
+//Конструктор инициализации объекта
     public Cheese(double amount, String sort, int fatPercent, String packaging) {
-        super("Сыр", amount);
+        super("Сыр", amount); //передача в родительский класс
         this.sort = sort;
         this.fatPercent = fatPercent;
         this.packaging = packaging;
     }
 
+//Получить сорт
     public String getSort() {
         return sort;
     }
 
+//Получить процент жирности
     public int getFatPercent() {
         return fatPercent;
     }
 
+//Получить тип упаковки
     public String getPackaging() {
         return packaging;
     }
