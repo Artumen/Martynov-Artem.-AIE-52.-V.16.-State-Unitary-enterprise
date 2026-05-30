a6c885c24e4331021a7ad3d19baca46f6d32a256
Artumen <969artumen123@mail.ru>
2026-05-31 01:35:19 +0700

    Delivery.java

--- src/Delivery.java
+++ src/Delivery.java
@@ -1,40 +1,52 @@
 public class Delivery {
+//Год 
     private int year;
+//Дата 
     private String date;
+//Поставщик
     private String supplier;
+//Массив продуктов в доставке
     private Product[] products;
+//текущее количество добавленных продуктов
     private int count;
 
+//конструктор для инициализации доставки
     public Delivery(int year, String date, String supplier, int maxSize) {
         this.year = year;
         this.date = date;
         this.supplier = supplier;
-        this.products = new Product[maxSize];
+        this.products = new Product[maxSize]; 
         this.count = 0;
     }
 
+//Получение года
     public int getYear() {
         return year;
     }
 
+//Получение даты
     public String getDate() {
         return date;
     }
 
+//получение поставщика
     public String getSupplier() {
         return supplier;
     }
 
+//Получение текущего количества продуктов
     public int getCount() {
         return count;
     }
 
+//Получение продукта по индексу
     public Product getProduct(int index) {
         return products[index];
     }
 
+//Добавление продукта в доставку
     public void add(Product product) {
-        if (count < products.length) {
+        if (count < products.length) { // Проверка на свободное место
             products[count] = product;
             count++;
         }
